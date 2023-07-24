a25fb663532d72a6fc08f9ff7c843a26e18d7f41
jobin <jobinjose.in@gmail.com>
2023-07-24 22:40:58 +0530

    JWT authentication

--- src/main/java/com/vishnu/Nectar/config/SecurityConfig.java
+++ src/main/java/com/vishnu/Nectar/config/SecurityConfig.java
@@ -1,22 +1,31 @@
 package com.vishnu.Nectar.config;
 
+import com.vishnu.Nectar.filter.JwtAuthFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthFilter authFilter;
 
     @Bean
     public UserDetailsService userDetailsService(){
@@ -27,9 +36,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf->csrf.disable())
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/user/addUser").permitAll()
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/user/login").permitAll()
                         .requestMatchers("/user/**").authenticated()
                 )
+                .sessionManagement((sessionManagement)->sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
                 .httpBasic(Customizer.withDefaults()).build();
     }
 
@@ -44,5 +56,9 @@ public class SecurityConfig {
         authenticationProvider.setUserDetailsService(userDetailsService());
         authenticationProvider.setPasswordEncoder(passwordEncoder());
         return  authenticationProvider;
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
     }
 }
--- src/main/java/com/vishnu/Nectar/controller/UserController.java
+++ src/main/java/com/vishnu/Nectar/controller/UserController.java
@@ -1,12 +1,18 @@
 package com.vishnu.Nectar.controller;
 
+import com.vishnu.Nectar.dto.AuthRequest;
 import com.vishnu.Nectar.dto.UserRequest;
 import com.vishnu.Nectar.dto.UserResponse;
 import com.vishnu.Nectar.entity.User;
+import com.vishnu.Nectar.service.JwtService;
 import com.vishnu.Nectar.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,14 +20,30 @@ import java.util.List;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private JwtService jwtService;
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
-    @PostMapping("/addUser")
+
+
+    @PostMapping("/signup")
     @PreAuthorize("hasAuthority('ROLE_ADMIN') or hasAuthority('ROLE_MANAGER')")
     public String createUser(@RequestBody User user){
         return userService.createUser(user);
+    }
+
+    @PostMapping("/login")
+    public String loginUser(@RequestBody AuthRequest authRequest){
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(),authRequest.getPassword()));
+        if(authentication.isAuthenticated()){
+            return jwtService.generateToken(authRequest.getUsername());
+        }else{
+            throw new UsernameNotFoundException("Invalid user request");
+        }
     }
 
     @PutMapping("/updateUser")
